4ab193d915f9d48f29d14bfcd4b8f39533987109
Tyler Shook <tshook426@gmail.com>
2025-09-08 22:20:46 -0400

    update lintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,37 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  env: {
+    browser: true,   // for React Native
+    es2021: true,    // latest JS features
+    node: true,      // allow Node globals like __dirname
+  },
+  extends: [
+    "eslint:recommended",      // base recommended rules
+    "plugin:react/recommended", // React rules
+    "plugin:react-native/all",  // React Native rules
+  ],
+  parserOptions: {
+    ecmaVersion: 12,      // modern JS
+    sourceType: "module", // use import/export
+    ecmaFeatures: {
+      jsx: true,          // enable JSX
+    },
+  },
+  plugins: [
+    "react",
+    "react-native",
+  ],
+  rules: {
+    // Common style rules you can tweak:
+    semi: ["error", "always"],         // require semicolons
+    quotes: ["error", "double"],       // enforce double quotes
+    "no-unused-vars": ["warn"],        // warn instead of error for unused vars
+    "react/prop-types": "off",         // turn off if not using PropTypes
+    "react-native/no-inline-styles": "off", // inline styles are common in RN
+  },
+  settings: {
+    react: {
+      version: "detect", // auto-detect React version
+    },
+  },
 };
